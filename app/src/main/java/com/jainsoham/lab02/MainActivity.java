b1c11454c776c5372acedd16fd453e3634ee7097
2025sjain <93287597+2025sjain@users.noreply.github.com>
2024-02-12 10:11:26 -0500

    Embellishment

--- app/src/main/java/com/jainsoham/lab02/MainActivity.java
+++ app/src/main/java/com/jainsoham/lab02/MainActivity.java
@@ -2,23 +2,29 @@ package com.jainsoham.lab02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Arrays;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     Button clickButton, planetButton;
     TextView textView, planetText;
+    ImageView img;
     EditText editText;
     String[] messages;
     String[] planets;
 
     int count = 1;
+    TextToSpeech t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +36,20 @@ public class MainActivity extends AppCompatActivity {
         planets = getResources().getStringArray(R.array.planets);
         planetButton = findViewById(R.id.planet_button);
         planetText = findViewById(R.id.planet_text);
+        img = findViewById(R.id.planet_image);
+        t1 = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.US);
+                }
+            }
+        });
         clickButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String text = editText.getText().toString();
+                t1.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 textView.setText(text);
                 editText.setText("");
                 String[] temp = new String[messages.length + 1];
@@ -47,7 +63,26 @@ public class MainActivity extends AppCompatActivity {
         planetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                planetText.setText(planets[count % 8]);
+                String planet = planets[count % 8];
+                t1.speak(planet, TextToSpeech.QUEUE_FLUSH, null);
+                planetText.setText(planet);
+                if (planet.equals("Mercury")) {
+                    img.setImageResource(R.drawable.mercury);
+                } else if (planet.equals("Venus")) {
+                    img.setImageResource(R.drawable.venus);
+                } else if (planet.equals("Earth")) {
+                    img.setImageResource(R.drawable.earth);
+                } else if (planet.equals("Mars")) {
+                    img.setImageResource(R.drawable.mars);
+                } else if (planet.equals("Jupiter")) {
+                    img.setImageResource(R.drawable.jupiter);
+                } else if (planet.equals("Saturn")) {
+                    img.setImageResource(R.drawable.saturn);
+                } else if (planet.equals("Uranus")) {
+                    img.setImageResource(R.drawable.uranus);
+                } else {
+                    img.setImageResource(R.drawable.neptune);
+                }
                 count++;
             }
         });
